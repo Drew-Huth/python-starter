25049db62cfa0f6cad2c2e0bb95afb4888cc89e6
Drew-Huth <147175635+Drew-Huth@users.noreply.github.com>
2023-11-08 16:39:52 +0000

    yeet

---
+++ Assignments/assignment8.py
@@ -0,0 +1,10 @@
+def main():
+    myList = [6,2,7,3,77,7,1]
+    minSum = myList[0] 
+    for num in myList:
+        if num < minSum:
+            minSum = num
+    print(minSum)
+
+if __name__ == '__main__':
+    main()
